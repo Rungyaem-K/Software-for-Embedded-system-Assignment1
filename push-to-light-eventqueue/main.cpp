6673e2b341e9733f1888f7a5174a4865078b7be0
Makma-Jantakarn <61446027+Makma-Jantakarn@users.noreply.github.com>
2020-02-25 15:57:43 +0700

    Update main.cpp

--- push-to-light-eventqueue/main.cpp
+++ push-to-light-eventqueue/main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-
 DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 InterruptIn button(PC_13);
@@ -7,20 +6,18 @@ EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
 
 void toggle() {   
-    led2=!led2;
+    led2=!led2;                                                         //led2 not equal led2
 }
 
 int main() {
-	led2=0;
+	led2=0;                                                         
 	t.start(callback(&queue, &EventQueue::dispatch_forever));
     	button.rise(toggle);     					//push to light
-   	button.fall(queue.event(toggle));	
-
+   	button.fall(queue.event(toggle));	                        // Release the button
 	while(1) {
          led1 = 1; 							// LED is ON
          wait(0.2); 							// 200 ms
          led1 = 0; 							// LED is OFF
          wait(0.8); 							// 800 ms
      		}
-
 }     
